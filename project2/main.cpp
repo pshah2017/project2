2c63b922d1f170f3f92e94cd50af97061ee78e49
PS <pshah20@gmail.com>
2017-11-04 19:00:49 -0400

    Initial Commit

---
+++ project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by Punit on 11/4/17.
+//  Copyright © 2017 Punit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
